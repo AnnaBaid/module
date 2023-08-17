56deb004fac5cdaa0d232b4c56a6246fbfecd6b1
AnnaBaid <danilova_any@ukr.net>
2023-08-17 20:55:11 +0200

    commit

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { StatusComponent } from './status/status.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NewsComponent,
+    TasksComponent,
+    StatusComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    NgFor,
+    MatCheckboxModule,
+    MatChipsModule
+  ]
+})
+export class HomeModule { }
